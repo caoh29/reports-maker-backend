efb898c180113eeb0b4a698ab96ada5413fa78a2
Camilo Ordoñez <cronox20@gmail.com>
2025-06-24 12:01:22 -0400

    erasing export entity on module

--- src/employee/employee.module.ts
+++ src/employee/employee.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { EmployeeService } from './employee.service';
 import { EmployeeController } from './employee.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
-import { EmployeeEntity } from './entities/employee.entity';
 
 @Module({
   controllers: [EmployeeController],
   providers: [EmployeeService],
   imports: [PrismaModule],
-  exports: [EmployeeService, EmployeeEntity],
+  exports: [EmployeeService],
 })
 export class EmployeeModule { }
